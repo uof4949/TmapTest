16bbfd8e73d7607497119ea3d05916ad5cf171df
JongBeom Chung <uof4949@naver.com>
2017-06-09 22:44:07 +0900

    ActivityCompat issues.

---
+++ app/src/main/java/kr/ac/gachon/tmaptest2/MainActivity.java
@@ -0,0 +1,237 @@
+package kr.ac.gachon.tmaptest2;
+
+
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.location.Location;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.PermissionChecker;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.skp.Tmap.TMapGpsManager;
+import com.skp.Tmap.TMapMarkerItem;
+import com.skp.Tmap.TMapPoint;
+import com.skp.Tmap.TMapView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity implements TMapGpsManager.onLocationChangedCallback {
+
+    private Context mContext = null;
+    private boolean m_bTrackingMode = true;
+
+    public final int MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION =1;
+    private TMapGpsManager tmapgps = null;
+    private TMapView tmapview = null;
+    private static String mApiKey = "eb2f6d74-4047-3adf-be50-928b2797d49e";
+    private static int mMarkerID;
+
+    private ArrayList<TMapPoint> m_tmapPoint = new ArrayList<TMapPoint>();
+    private ArrayList<String> mArrayMarkerID = new ArrayList<String>();
+    private ArrayList<MapPoint> m_mapPoint = new ArrayList<MapPoint>();
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
+
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+
+                } else {
+
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                }
+                return;
+            }
+
+            // other 'case' lines to check for other
+            // permissions this app might request
+        }
+    }
+
+    @Override
+    public void onLocationChange(Location location) {
+        if (m_bTrackingMode) {
+            tmapview.setLocationPoint(location.getLongitude(), location.getLatitude());
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mContext = this;
+
+        LinearLayout linearLayout = (LinearLayout) findViewById(R.id.mapview);
+        tmapview = new TMapView(this);
+        linearLayout.addView(tmapview);
+        tmapview.setSKPMapApiKey(mApiKey);
+
+        addPoint();
+        showMarkerPoint();
+
+        // Here, thisActivity is the current activity
+        if (PermissionChecker.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_FINE_LOCATION)) {
+
+                // Show an expanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+
+            } else {
+
+                // No explanation needed, we can request the permission.
+
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                        MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
+
+                // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
+                // app-defined int constant. The callback method gets the
+                // result of the request.
+            }
+        }
+        // 현재 보는 방향
+        tmapview.setCompassMode(true);
+
+        // 현위치 아이콘표시
+        tmapview.setIconVisibility(true);
+
+        // 줌레벨
+        tmapview.setZoomLevel(15);
+        tmapview.setMapType(TMapView.MAPTYPE_STANDARD);
+        tmapview.setLanguage(TMapView.LANGUAGE_KOREAN);
+
+        tmapgps = new TMapGpsManager(MainActivity.this);
+        tmapgps.setMinTime(1000);
+        tmapgps.setMinDistance(5);
+        tmapgps.setProvider(tmapgps.NETWORK_PROVIDER);
+        // 연결된 인터넷으로 현 위치를 받는다.
+        // 실내일 때 유용함.
+        // tmapgps.setProvider(tmapgps.GPS_PROVIDER); // gps로 현 위치를 잡습니다.
+
+        tmapgps.OpenGps();
+
+        // 화면중심을 단말의 현재위치로 이동.
+        tmapview.setTrackingMode(true);
+        tmapview.setSightVisible(true);
+
+        // 풍선에서 우측 버튼 클릭시 할 행동입니다.
+        tmapview.setOnCalloutRightButtonClickListener(new TMapView.OnCalloutRightButtonClickCallback() {
+            @Override
+            public void onCalloutRightButton(TMapMarkerItem markerItem) {
+                Toast.makeText(MainActivity.this, "클릭", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
+    }
+
+    public void addPoint() { // 여기에 핀을 꼽을 포인트들을 배열에 add해주세요!
+        // 강남
+        m_mapPoint.add(new MapPoint("강남", 37.510350, 127.066847));
+    }
+
+    public void showMarkerPoint() { // 마커 찍는거 빨간색 포인트.
+        for (int i = 0; i < m_mapPoint.size(); i++) {
+            TMapPoint point = new TMapPoint(m_mapPoint.get(i).getLatitude(),
+                    m_mapPoint.get(i).getLongitude());
+            TMapMarkerItem item1 = new TMapMarkerItem();
+            Bitmap bitmap = null;
+            bitmap = BitmapFactory.decodeResource(mContext.getResources(), R.mipmap.poi_dot);
+            // poi_dot은 지도에 꼽을 빨간 핀 이미지입니다.
+
+            item1.setTMapPoint(point);
+            item1.setName(m_mapPoint.get(i).getName());
+            item1.setVisible(item1.VISIBLE);
+
+            item1.setIcon(bitmap);
+
+            bitmap = BitmapFactory.decodeResource(mContext.getResources(), R.mipmap.poi_dot);
+
+            // 풍선뷰 안의 항목에 글을 지정합니다.
+            item1.setCalloutTitle(m_mapPoint.get(i).getName());
+            item1.setCalloutSubTitle("서울");
+            item1.setCanShowCallout(true);
+            item1.setAutoCalloutVisible(true);
+
+            Bitmap bitmap_i = BitmapFactory.decodeResource(mContext.getResources(), R.mipmap.i_go);
+
+            item1.setCalloutRightButtonImage(bitmap_i);
+
+            String strID = String.format("pmarker%d", mMarkerID++);
+
+            tmapview.addMarkerItem(strID, item1);
+            mArrayMarkerID.add(strID);
+
+        }
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Main Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app URL is correct.
+                Uri.parse("android-app://kr.ac.gachon.tmaptest2/http/host/path")
+        );
+        AppIndex.AppIndexApi.start(client, viewAction);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Main Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app URL is correct.
+                Uri.parse("android-app://kr.ac.gachon.tmaptest2/http/host/path")
+        );
+        AppIndex.AppIndexApi.end(client, viewAction);
+        client.disconnect();
+    }
+}
